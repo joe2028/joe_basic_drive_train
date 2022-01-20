9c7f6f3a455b6dd7658a9fe7cedd3af9ac550744
MavG2 <96135184+joe2028@users.noreply.github.com>
2022-01-20 15:20:04 -0500

    initial commit

---
+++ molly_drive_train-Imported/src/main/java/frc/robot/commands/DriveCommand.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+//Joysticks X and Y
+import frc.robot.subsystems.Drivetrain;
+
+
+public class DriveCommand extends CommandBase {
+  public DoubleSupplier joystickInputX;
+  public DoubleSupplier joystickInputY;
+
+  private Drivetrain driveSubsystem;
+  /** Creates a new DriveCommand. */
+  public DriveCommand(DoubleSupplier inputX, DoubleSupplier inputY, Drivetrain driveTrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    joystickInputX = inputX;
+    joystickInputY = inputY;
+    driveSubsystem = driveTrain;
+    addRequirements(driveSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    driveSubsystem.diffDrive(joystickInputX.getAsDouble(), joystickInputY.getAsDouble());
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    driveSubsystem.diffDrive(joystickInputX.getAsDouble(), joystickInputY.getAsDouble());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
